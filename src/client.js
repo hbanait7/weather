69c5bd87049e2bbe45fe9d272ed70861c99b6342
hbanait <hbanait7@gmail.com>
2016-03-16 20:47:58 -0400

    Final

---
+++ src/client.js
@@ -0,0 +1,42 @@
+function getWeather() {
+    var city = document.getElementById("city").value
+    var country = document.getElementById("country").value
+    console.log(city + ' ' + country)
+    const API_Key = 'b948a94521a4390a'
+    let query = city + ' ' + country
+    const apiURL = `https://api.wunderground.com/api/${API_Key}/conditions/q/${query}.json`
+
+    fetch (apiURL).then(function(resp) {
+          console.log(resp.type)
+        resp.json().then(function(result) {
+          console.log(result)
+        let curObs = result.current_observation
+
+        document.getElementById('result').style.display= 'block'
+
+        let loc = document.getElementById('location').innerHTML = curObs.display_location.city + ', ' + curObs.display_location.country
+
+        let img = document.createElement('img')
+          img.src = curObs.icon_url
+          document.getElementById('icon').appendChild(img)
+
+        let temp = document.getElementById('temp').innerHTML = curObs.temp_c + '&#8451'
+
+        let weather = document.getElementById('weather').innerHTML = curObs.weather
+
+        let feelsLike = document.getElementById('feels-like').innerHTML = 'Feels Like: ' + curObs.feelslike_c + '&#8451'
+
+        let wind = document.getElementById('wind').innerHTML = 'Wind: ' + curObs.wind_string
+
+        })
+
+    })
+
+}
+
+
+
+// ${} == erb <%= %>
+// wikipedia api
+// rails project show list of users + login count
+// initialize project inside
